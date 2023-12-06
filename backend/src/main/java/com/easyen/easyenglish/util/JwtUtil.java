482bc37cae9b471978ab422f44418518c9952ba3
AkashiRoot <129533499+AkashiRoot@users.noreply.github.com>
2023-12-06 18:22:38 +0800

    JWT工具及拦截

---
+++ backend/src/main/java/com/easyen/easyenglish/util/JwtUtil.java
@@ -0,0 +1,67 @@
+package com.easyen.util;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.easyen.entitty.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JwtUtil {
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtil.class);
+    /**
+     * 密钥
+     */
+    private static final String SECRET = "my_secret";
+
+    /**
+     * 过期时间
+     **/
+    private static final long EXPIRATION = 14400L;//单位为秒，4小时
+
+    /**
+     * 生成用户token,设置token超时时间
+     */
+    public static String createToken(User user) {
+        //过期时间
+        Date expireDate = new Date(System.currentTimeMillis() + EXPIRATION * 1000);
+        Map<String, Object> map = new HashMap<>();
+        map.put("alg", "HS256");
+        map.put("typ", "JWT");
+        String token = JWT.create()
+                .withHeader(map)// 添加头部
+                //将基本信息放到claims中
+                .withClaim("id", user.getUser_id())//userId
+                .withClaim("username", user.getUser_name())//userName
+                .withClaim("password", user.getPassword())//password
+                .withClaim("email", user.getEmail())
+                .withClaim("other_info", user.getOther_info())
+                //.withClaim("account_lock_status", user.isAccountLockStatus())
+                .withExpiresAt(expireDate) //超时设置,设置过期的日期
+                .withIssuedAt(new Date()) //签发时间
+                .sign(Algorithm.HMAC256(SECRET)); //SECRET加密
+        return token;
+    }
+
+
+    /**
+     * 校验token并解析token
+     */
+    public static Map<String, Claim> verifyToken(String token) {
+        DecodedJWT jwt = null;
+        try {
+            JWTVerifier verifier = JWT.require(Algorithm.HMAC256(SECRET)).build();
+            jwt = verifier.verify(token);
+
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            logger.error("token解码异常");
+            return null;
+        }
+        return jwt.getClaims();
+    }
+}
