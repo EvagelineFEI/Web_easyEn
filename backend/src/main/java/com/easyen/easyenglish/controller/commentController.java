14a13fe8c4313c38fc89b5b319f52ee2df4629da
PolaKuma <1356732652@qq.com>
2023-12-10 21:55:13 +0800

    加了一下url

--- backend/src/main/java/com/easyen/easyenglish/controller/commentController.java
+++ backend/src/main/java/com/easyen/easyenglish/controller/commentController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/comment")
 public class commentController {
     @Autowired
     commentService commentService;
--- backend/src/main/java/com/easyen/easyenglish/controller/postController.java
+++ backend/src/main/java/com/easyen/easyenglish/controller/postController.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api/post")
 public class postController {
     @Autowired
     postService postService;
